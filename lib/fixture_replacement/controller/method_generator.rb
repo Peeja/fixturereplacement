63a6dfaf5b2dd2cbea341369e0228163b8cf149a
Peter Jaros <peeja@dropio.com>
2008-10-10 10:33:22 -0400

    Add attributes_for_* methods.

--- lib/fixture_replacement/controller/method_generator.rb
+++ lib/fixture_replacement/controller/method_generator.rb
@@ -17,6 +17,7 @@ module FixtureReplacementController
       generate_default_method
       generate_new_method
       generate_create_method
+      generate_attributes_method
     end
     
     def generate_default_method
@@ -48,6 +49,24 @@ module FixtureReplacementController
       ClassFactory.fixture_replacement_module.module_eval do
         define_method("new_#{obj.fixture_name}") do |*args|
           obj.to_new_class_instance(args[0], self)
+        end
+      end
+    end
+    
+    # Generates +attributes_for_*+ methods which returns the
+    # attributes given in example_data.rb as a hash.  If attribute
+    # names are given to the generated method, only those
+    # attributes are returned.
+    def generate_attributes_method
+      obj = @object_attributes
+
+      ClassFactory.fixture_replacement_module.module_eval do
+        define_method("attributes_for_#{obj.fixture_name}") do |*args|
+          if args.size > 1
+            obj.hash.slice(*args)
+          else
+            obj.hash
+          end
         end
       end
     end
